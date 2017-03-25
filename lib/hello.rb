eb6af1617a25761d365aea1a4c974668c00d3b18
Paisley Singh <paisley.singh@gmail.com>
2017-03-25 03:34:18 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,33 @@
-def hello_t
+# def hello_t(array)
+#   if block_given?
+#     i = 0
+#     while i < array.length
+#       yield(array[i])
+#       i = i + 1
+#     end
+#     array
+#   else
+#     puts "Hey! No block was given!"
+#   end
+# end
+#
+# # call your method here!
+#
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#     if name.start_with?("T")
+#         puts "Hi, #{name}"
+#     end
+# end
 
+# followed the lesson, but the tests were not passing.
+
+def hello_t(array)
+   temp = []
+   array.each do |name|
+     if name.start_with?("T") || name.start_with?("t")
+       puts "Hi, #{name}"
+       temp << name
+     end
+   end
+   temp
 end
-
-# call your method here!
-
